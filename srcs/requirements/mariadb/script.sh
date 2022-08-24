fb35e3866f13a5fe989349964c3d8b6f7853f13e
Simon Gaubert <sim.gaubert.sg@gmail.com>
2022-08-24 10:24:10 +0200

    je sais plus

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,18 +1,13 @@
 touch tmp_file
 chmod 755 tmp_file
 
-cat << stop_file > tmp_file
-CREATE DATABASE inception;
-USE inception;
-FLUSH PRIVILEGES ;
-GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$MYSQL_ROOT_PASSWORD') ;
-FLUSH PRIVILEGES ;
-GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-DROP DATABASE IF EXISTS test ;
-FLUSH PRIVILEGES ;
-stop_file
+cat << EOF > tmp_file
+CREATE DATABASE IF NOT EXISTS inception
+GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'cookie42admin' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
 
-mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < tmp_file
-exec mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 --verbose=1 $@
+cat tmp_file
+
+mysqld < tmp_file
+usr/bin/mysqld_safe --datadir=/var/lib/mysql
